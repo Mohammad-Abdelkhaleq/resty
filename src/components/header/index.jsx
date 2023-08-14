2afc3432641bf023a46a82f64426e0e1ea203493
Mohammad-Abdelkhaleq <alamrimohammad959@gmail.com>
2023-08-14 11:18:23 +0000

    converting the class components into a functional components

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>RESTy</h1>
-      </header>
-    );
-  }
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>RESTy</h1>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Header;
+
+// use function components instead of class components  
+
+ function Header(props) {
+  return (
+    <header>
+      <h1>RESTy</h1>
+    </header>
+  );
+
 }
 
 export default Header;
